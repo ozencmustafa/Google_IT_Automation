1271855bec54a4ea3e43f6c088f9c10db1dce3c6
ozencmustafa <ozencmustafa@hotmail.com>
2022-03-23 00:24:24 +0100

    Python with SQLite added

---
+++ Using_Databases_with_Python/week2/main.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+# CREATE TABLE Users(name VARCHAR(128),email VARCHAR(128))
+# INSERT or IGNORE INTO Users(name, email) VALUES("kristin", "krs@umich.eu")
+# DELETE FROM Users WHERE email='ted@umich.eu"
+# UPDATE Users SET name="charles" WHERE email="vit@umich.eu"
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox-short.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    org = email.split('@')[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
